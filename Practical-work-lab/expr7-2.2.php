470aa8a9276b00bb45b723aefa389f0ef4fb6588
Dhruvil-prajapati-16 <151436113+Dhruvil-prajapati-16@users.noreply.github.com>
2024-03-05 21:29:10 +0530

    exprwithout constructor

---
+++ Practical-work-lab/expr7-2.2.php
@@ -0,0 +1,35 @@
+<?
+class Person {
+    private $name;
+    private $age;
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setAge($age) {
+        $this->age = $age;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+}
+
+$person = new Person();
+
+// Set the person's name using the set attribute method
+$person->setName('Jane Doe');
+
+// Get the person's name using the get attribute method
+echo $person->getName(); // Outputs 'Jane Doe'
+
+// Set the person's age using the set attribute method
+$person->setAge(31);
+
+// Get the person's age using the get attribute method
+echo $person->getAge(); // Outputs 31
