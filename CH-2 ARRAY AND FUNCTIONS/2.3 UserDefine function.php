a14426086ec495116ee3083b65a0cada3fbe62d0
Dhruvil-prajapati-16 <151436113+Dhruvil-prajapati-16@users.noreply.github.com>
2024-03-10 19:00:57 +0530

    #

---
+++ CH-2 ARRAY AND FUNCTIONS/2.3 UserDefine function.php
@@ -0,0 +1,18 @@
+<?
+// user define fumction
+
+function display()
+{
+    echo "hello World";
+}
+
+function displayParameter($a)
+{
+    echo "hello $a , \n how are you ";
+}
+
+display();
+displayParameter("Dhruv");
+error_reporting(E_ALL);
+
+?>
